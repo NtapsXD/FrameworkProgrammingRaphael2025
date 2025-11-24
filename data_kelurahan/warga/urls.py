28b05de1ce49dabbe7b91f94602e0742e9022b69
NtapsXD <raphaelsosiawan64@gmail.com>
2025-11-24 10:18:25 +0700

    Pertemuan 1 Framework Programming

---
+++ data_kelurahan/warga/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import WargaListView, WargaDetailView, PengaduanListView
+
+urlpatterns = [
+    path('', WargaListView.as_view(), name='warga-list'),
+    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+]
