f9e20d7faae86722c94a99da856082c892f55860
NtapsXD <raphaelsosiawan64@gmail.com>
2025-11-24 11:22:07 +0700

    Pertemuan 2 Framework Programming

---
+++ warga/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView, DetailView
+from .models import Warga, Pengaduan
+
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
